165e6e23d1d152b70b58852f24f2f331ed5417bc
Florry <soderstrom.viktor@gmail.com>
2024-12-21 19:39:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Supported properties (on static mesh actors)
+
+- Location
+- Rotation
+- Scale
+
+### Usage
+
+0. Copy actor from UDK
+1. Run script
+2. Paste `Paste All Properties in Category` on `Transform` category on actor in UE5
---
+++ index.mjs
@@ -0,0 +1,36 @@
+import clipboard from "clipboardy";
+
+const clipboardContents = clipboard.readSync();
+
+const location = clipboardContents.match(/Location=\(X=(.*),Y=(.*),Z=(.*)\)/);
+const locationString = `Location=(X=${location?.[1]},Y=${location?.[2]},Z=${location?.[3]})`;
+
+const rotation = clipboardContents.match(
+  /Rotation=\(Pitch=(.*),Yaw=(.*),Roll=(.*)\)/
+);
+const pitchInDegrees = (Number(rotation?.[1] ?? 0) / 65536) * 360;
+const yawInDegrees = (Number(rotation?.[2] ?? 0) / 65536) * 360;
+const rollInDegrees = (Number(rotation?.[3] ?? 0) / 65536) * 360;
+const rotationString = `Rotation=(Pitch=${pitchInDegrees},Yaw=${yawInDegrees},Roll=${rollInDegrees})`;
+
+const scale = clipboardContents.match(/DrawScale3D=\(X=(.*),Y=(.*),Z=(.*)\)/);
+const scaleString = `DrawScale3D=(X=${scale?.[1]},Y=${scale?.[2]},Z=${scale?.[3]})`;
+
+const output = `{
+  "Tagged": [
+    [
+      "RelativeLocation",
+      "${locationString}"
+    ],
+    [
+      "RelativeRotation",
+      "${rotationString}"
+    ],
+    [
+      "RelativeScale3D",
+      "${scaleString}"
+    ]
+  ]
+}`;
+
+clipboard.writeSync(output);
