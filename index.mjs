e6c725b13af917bf934b17a7211e3758450dedaf
Florry <soderstrom.viktor@gmail.com>
2024-12-21 19:48:51 +0100

    fix

--- index.mjs
+++ index.mjs
@@ -14,7 +14,9 @@ const rollInDegrees = (Number(rotation?.[3] ?? 0) / 65536) * 360;
 const rotationString = `Rotation=(Pitch=${pitchInDegrees},Yaw=${yawInDegrees},Roll=${rollInDegrees})`;
 
 const scale = clipboardContents.match(/DrawScale3D=\(X=(.*),Y=(.*),Z=(.*)\)/);
-const scaleString = `DrawScale3D=(X=${scale?.[1]},Y=${scale?.[2]},Z=${scale?.[3]})`;
+const scaleString = scale
+  ? `DrawScale3D=(X=${scale[1]},Y=${scale[2]},Z=${scale[3]})`
+  : "DrawScale3D=(X=1,Y=1,Z=1)";
 
 const output = `{
   "Tagged": [
